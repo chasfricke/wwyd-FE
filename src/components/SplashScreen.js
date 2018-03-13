85bd12ee992466f5e5e63a0c2e2f8ba8382ec1d3
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-03-13 14:02:38 -0600

    updated splashscreen.js header

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -5,11 +5,10 @@ export class SplashScreen extends React.Component {
   render () {
     return (
       <div >
-          
+          <div className="title-div">
+            <h2>HOW TO PLAY</h2>
+          </div>
           <div className="instructions-container">
-            <div className="title-div">
-              <h2>HOW TO PLAY</h2>
-            </div>
             <h3>OVERVIEW</h3>
             <p><i>What Would You Do?</i> is collaborative group game designed to initiate meaningful conversation amongst adults.  Watch as even the most shy of the bunch can't help but weigh in on these wacky hypotheticals. The only question left to answer is, What would you do?</p>
             <h3>REQUIREMENTS</h3>
