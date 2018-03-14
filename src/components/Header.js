4d2494c9105bde19a2e0e69fdee262c051fffb78
Rossprehn <32393462+Rossprehn@users.noreply.github.com>
2018-03-14 14:46:57 -0600

    pulling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,16 @@
-import React from "react";
-import '../css/Header.css';
+import React from 'react'
+import '../css/Header.css'
 
 export class Header extends React.Component {
-  render () {
+  render() {
     return (
       <header>
         <img className="header-image" src="./assets/logo.png" alt="logo" />
-<<<<<<< HEAD
-=======
+
         <img className="header-title" src="./assets/title.png" alt="logo" />
 
-
         <img className="hamburger-button" src="./assets/hamburger-button.png" alt="button" />
->>>>>>> 2c8c11a1c4b614a248bb74ee184db1195e558c70
       </header>
-    );
+    )
   }
 }
